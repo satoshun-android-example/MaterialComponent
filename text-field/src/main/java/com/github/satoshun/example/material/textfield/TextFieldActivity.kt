722eda31d79635d962ba8b71adc6058522a7a09d
SatoShun <shun.sato1@gmail.com>
2018-09-11 23:09:57 +0000

    material component first

---
+++ text-field/src/main/java/com/github/satoshun/example/material/textfield/TextFieldActivity.kt
@@ -0,0 +1,11 @@
+package com.github.satoshun.example.material.textfield
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TextFieldActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.material_text_field)
+  }
+}
