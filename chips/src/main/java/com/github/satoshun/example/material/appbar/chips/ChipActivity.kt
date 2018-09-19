97b3c9ff2b778a0693a3a8e7f0174686f6e4c74d
SatoShun <shun.sato1@gmail.com>
2018-09-19 00:37:00 +0000

    checkable chip

--- chips/src/main/java/com/github/satoshun/example/material/appbar/chips/ChipActivity.kt
+++ chips/src/main/java/com/github/satoshun/example/material/appbar/chips/ChipActivity.kt
@@ -3,7 +3,7 @@ package com.github.satoshun.example.material.appbar.chips
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class ChipBottomActivity : AppCompatActivity() {
+class ChipActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.chip_act)
