d3f074440fcf4313d8a865a7af5bd7d9e97163ee
SatoShun <shun.sato1@gmail.com>
2018-09-20 00:48:04 +0000

    editable

--- chips/src/main/java/com/github/satoshun/example/material/appbar/chips/ChipFragment.kt
+++ chips/src/main/java/com/github/satoshun/example/material/appbar/chips/ChipFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import androidx.core.view.postDelayed
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.chip_frag.*
 
@@ -24,6 +26,23 @@ class ChipFragment : Fragment() {
     }
     input_chip2.setOnCloseIconClickListener {
       input_groups.removeView(input_chip2)
+    }
+
+    editable.setOnEditorActionListener { _, actionId, _ ->
+      if (actionId == EditorInfo.IME_ACTION_DONE) {
+        editable_chip.text = editable.editableText.toString()
+        editable_chip.visibility = View.VISIBLE
+        editable.visibility = View.GONE
+        true
+      } else {
+        false
+      }
+    }
+    editable_chip.setOnClickListener {
+      it.postDelayed(50) {
+        editable_chip.visibility = View.GONE
+        editable.visibility = View.VISIBLE
+      }
     }
   }
 }
