84ccf0d1a1c620096fb21e5d4b04bc6f1c8fd7fb
SatoShun <shun.sato1@gmail.com>
2019-02-28 04:39:44 +0000

    use coordinator layout

--- bottomsheets/src/main/java/com/github/satoshun/example/material/bottomsheets/BottomSheetsActivity.kt
+++ bottomsheets/src/main/java/com/github/satoshun/example/material/bottomsheets/BottomSheetsActivity.kt
@@ -5,9 +5,7 @@ import android.util.DisplayMetrics
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.ProgressBar
-import android.widget.TextView
-import android.widget.Toolbar
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -16,7 +14,6 @@ import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
-import kotlinx.android.synthetic.main.bottomsheets_layout2.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
@@ -73,6 +70,16 @@ class BottomSheetsActivity : AppCompatActivity() {
             progress.isVisible = false
           }
 
+          val submit = dialog2.findViewById<Button>(R.id.submit)!!
+          submit.setOnClickListener {
+            GlobalScope.launch(Dispatchers.Main) {
+              progress.isVisible = true
+              postIds(adapter.checkboxedItemIds())
+              progress.isVisible = false
+              dialog2.dismiss()
+            }
+          }
+
           dialog2.show()
           dialog.dismiss()
         }
@@ -84,6 +91,11 @@ class BottomSheetsActivity : AppCompatActivity() {
   }
 }
 
+suspend fun postIds(ids: List<Int>) {
+  delay(3000)
+  // ...
+}
+
 class AdapterSample : GroupAdapter<ViewHolder>() {
   suspend fun populateItems() {
     delay(2000)
@@ -91,12 +103,34 @@ class AdapterSample : GroupAdapter<ViewHolder>() {
       (0..100).map { ItemSample(it) }
     )
   }
+
+  fun checkboxedItemIds(): List<Int> {
+    return (0 until itemCount).mapNotNull {
+      (getItem(it) as? ItemSample)
+        ?.takeIf { item -> item.isCheckboxed }
+        ?.itemId
+    }
+  }
+
+  fun isCheckBoxed(): Boolean {
+    return checkboxedItemIds().isNotEmpty()
+  }
 }
 
-class ItemSample(private val id: Int) : Item<ViewHolder>() {
+class ItemSample(
+  val itemId: Int
+) : Item<ViewHolder>() {
+
+  var isCheckboxed: Boolean = false
+    private set
+
   override fun getLayout(): Int = R.layout.bottomsheets_item
 
   override fun bind(viewHolder: ViewHolder, position: Int) {
     viewHolder.root.findViewById<TextView>(R.id.title).text = id.toString()
+    viewHolder.root.findViewById<CheckBox>(R.id.checkbox).isChecked = isCheckboxed
+    viewHolder.root.findViewById<CheckBox>(R.id.checkbox).setOnCheckedChangeListener { _, isChecked ->
+      this.isCheckboxed = isChecked
+    }
   }
 }
