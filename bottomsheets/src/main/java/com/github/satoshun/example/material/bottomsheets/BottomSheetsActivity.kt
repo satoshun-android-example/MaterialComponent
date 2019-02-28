518f17933c02a1912f3396ca150bd7586c34aa5a
SatoShun <shun.sato1@gmail.com>
2019-02-28 02:11:32 +0000

    first bottomsheet

---
+++ bottomsheets/src/main/java/com/github/satoshun/example/material/bottomsheets/BottomSheetsActivity.kt
@@ -0,0 +1,11 @@
+package com.github.satoshun.example.material.bottomsheets
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class BottomSheetsActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.bottomsheets_act)
+  }
+}
