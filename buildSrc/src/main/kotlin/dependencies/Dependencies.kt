c1a0e2e668a5a376797f5d799ba3fb6b00008264
SatoShun <shun.sato1@gmail.com>
2019-03-10 04:41:49 +0000

    use new buildSrc

---
+++ buildSrc/src/main/kotlin/dependencies/Dependencies.kt
@@ -0,0 +1,47 @@
+package dependencies
+
+const val COMPILE_SDK = 28
+const val MIN_SDK = 24
+const val TARGET_SDK = 28
+
+const val VKOTLIN = "1.3.21"
+const val VCOUROUTINE = "1.1.1"
+
+const val ANDROID_PLUGIN = "com.android.tools.build:gradle:3.4.0-rc01"
+const val KOTLIN_PLUGIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:$VKOTLIN"
+const val VERSIONS_PLUGIN = "com.github.ben-manes:gradle-versions-plugin:0.20.0"
+
+const val KOTLIN = "org.jetbrains.kotlin:kotlin-stdlib:$VKOTLIN"
+const val COROUTINE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$VCOUROUTINE"
+const val UI_COROUTINE = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$VCOUROUTINE"
+
+const val KTX = "androidx.core:core-ktx:1.0.1"
+
+const val APPCOMPAT = "androidx.appcompat:appcompat:1.0.2"
+const val RECYCLERVIEW = "androidx.recyclerview:recyclerview:1.0.0"
+const val CONSTRAINTLAYOUT = "androidx.constraintlayout:constraintlayout:2.0.0-alpha2"
+const val CARDVIEW = "androidx.cardview:cardview:1.0.0"
+
+const val MATERIAL = "com.google.android.material:material:1.1.0-alpha03"
+const val COORDINATORLAYOUT = "androidx.coordinatorlayout:coordinatorlayout:1.0.0"
+
+const val VIEWMODEL = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.0.0"
+const val LIVEDATA = "androidx.lifecycle:lifecycle-livedata:2.0.0"
+const val LIFECYCLE_COMPILER = "androidx.lifecycle:lifecycle-compiler:2.0.0"
+
+const val EMOJI = "androidx.emoji:emoji:1.0.0"
+const val EMOJI_COMPAT = "androidx.emoji:emoji-appcompat:1.0.0"
+const val EMOJI_BUNDLED = "androidx.emoji:emoji-bundled:1.0.0"
+
+const val GROUPIE = "com.xwray:groupie:2.3.0"
+
+const val COROUTINE_BINDING = "com.github.satoshun.coroutinebinding:coroutinebinding:1.0.0-RC1"
+
+const val RXJAVA = "io.reactivex.rxjava2:rxjava:2.2.0"
+const val RXANDROID = "io.reactivex.rxjava2:rxandroid:2.1.0"
+
+const val JUNIT = "junit:junit:4.12"
+const val TRUTH = "com.google.truth:truth:0.39"
+const val MOCKITO_KOTLIN = "com.nhaarman:mockito-kotlin-kt1.1:1.5.0"
+const val TEST_RUNNER = "androidx.test:runner:1.1.1-alpha01"
+const val ESPRESSO = "androidx.test.espresso:espresso-core:3.1.1-alpha01"
