51b50e07ef699f029ca8bfa553f4c4e003126074
SatoShun <shun.sato1@gmail.com>
2019-03-10 04:46:45 +0000

    floating first

---
+++ extended-floating/src/main/java/com/github/satoshun/example/material/floatingactionbutton/extended/BannerFragment.kt
@@ -0,0 +1,30 @@
+package com.github.satoshun.example.material.floatingactionbutton.extended
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.banner_frag.*
+
+class BannerFragment : Fragment() {
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View {
+    return inflater.inflate(R.layout.banner_frag, container, false)
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    fix_it.setOnClickListener {
+      (banner.parent as ViewGroup).removeView(banner)
+    }
+
+    learn_button.setOnClickListener {
+      (banner.parent as ViewGroup).removeView(banner)
+    }
+  }
+}
---
+++ extended-floating/src/main/java/com/github/satoshun/example/material/floatingactionbutton/extended/ExtendedFloatingButtonExampleActivity.kt
@@ -0,0 +1,20 @@
+package com.github.satoshun.example.material.floatingactionbutton.extended
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.banner_act.*
+
+class ExtendedFloatingButtonExampleActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.banner_act)
+    setSupportActionBar(toolbar)
+
+    if (savedInstanceState == null) {
+      supportFragmentManager
+          .beginTransaction()
+          .add(R.id.container, BannerFragment())
+          .commit()
+    }
+  }
+}
