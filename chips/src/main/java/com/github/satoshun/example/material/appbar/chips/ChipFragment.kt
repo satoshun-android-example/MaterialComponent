fa645beeebc3016b14c188be773754f21bd65279
SatoShun <shun.sato1@gmail.com>
2019-04-25 07:24:45 +0000

    sppanble

--- chips/src/main/java/com/github/satoshun/example/material/appbar/chips/ChipFragment.kt
+++ chips/src/main/java/com/github/satoshun/example/material/appbar/chips/ChipFragment.kt
@@ -1,12 +1,20 @@
 package com.github.satoshun.example.material.appbar.chips
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.style.AbsoluteSizeSpan
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import androidx.core.view.postDelayed
 import androidx.fragment.app.Fragment
+import com.google.android.material.chip.Chip
 import kotlinx.android.synthetic.main.chip_frag.*
 
 class ChipFragment : Fragment() {
@@ -41,5 +49,27 @@ class ChipFragment : Fragment() {
         editable.visibility = View.VISIBLE
       }
     }
+
+    spannable1.setTextWithNumber("Hello", 1000)
   }
+}
+
+
+fun Chip.setTextWithNumber(text: String, number: Long) {
+  val span = SpannableString(text + number.toString())
+  span.setSpan(
+//    StyleSpan(R.style.TextAppearance_MaterialComponents_Headline1),
+    AbsoluteSizeSpan(16, true),
+    0,
+    text.length,
+    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+  )
+  span.setSpan(
+    AbsoluteSizeSpan(10, true),
+    text.length,
+    text.length + number.toString().length,
+    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+  )
+
+  setText(span)
 }
