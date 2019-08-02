c3a9342637cc9a4e8af85d240eb7c177ea1d154a
SatoShun <shun.sato1@gmail.com>
2019-08-02 02:05:10 +0000

    update

---
+++ button/src/main/java/com/github/satoshun/example/material/appbar/button/BottomButton.kt
@@ -0,0 +1,15 @@
+package com.github.satoshun.example.material.appbar.button
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+import androidx.annotation.AttrRes
+
+class BottomButton @JvmOverloads constructor(
+  context: Context,
+  attrs: AttributeSet? = null,
+  @AttrRes defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+  init {
+  }
+}
--- button/src/main/java/com/github/satoshun/example/material/appbar/button/ButtonFragment.kt
+++ button/src/main/java/com/github/satoshun/example/material/appbar/button/ButtonFragment.kt
@@ -4,45 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isGone
-import androidx.core.view.isVisible
-import com.github.satoshun.coroutinebinding.view.awaitClick
-import kotlinx.android.synthetic.main.button_frag.*
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
 class ButtonFragment : BaseFragment() {
   override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
     savedInstanceState: Bundle?
-  ): View {
-    return inflater.inflate(R.layout.button_frag, container, false)
-  }
+  ): View =
+    inflater.inflate(
+      R.layout.button_frag,
+      container,
+      false
+    )
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
-
-    GlobalScope.launch {
-      while (true) {
-        expand.awaitClick()
-        expand.isGone = true
-
-        expanded_title.isVisible = true
-        collapse.isVisible = true
-        action_group.isVisible = true
-      }
-    }
-
-    GlobalScope.launch {
-      while (true) {
-        collapse.awaitClick()
-        expanded_title.isGone = true
-        collapse.isGone = true
-        action_group.isGone = true
-
-        expand.isVisible = true
-      }
-    }
   }
 }
