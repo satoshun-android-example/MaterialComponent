4c6a34dae48c750705f4176a909fef54d57436a2
SatoShun <shun.sato1@gmail.com>
2019-10-24 01:17:41 +0000

    use package private extension method

--- text-field/src/main/java/com/github/satoshun/example/material/textfield/TextFieldActivity.kt
+++ text-field/src/main/java/com/github/satoshun/example/material/textfield/TextFieldActivity.kt
@@ -1,9 +1,9 @@
 package com.github.satoshun.example.material.textfield
 
-import android.graphics.drawable.AnimatedStateListDrawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputLayout
+import com.google.android.material.textfield.endIconView
 
 
 class TextFieldActivity : AppCompatActivity() {
@@ -12,26 +12,7 @@ class TextFieldActivity : AppCompatActivity() {
     setContentView(R.layout.material_text_field)
 
     val basic = findViewById<TextInputLayout>(R.id.basic)
-    val endIcon = basic.endIconDrawable as AnimatedStateListDrawable
-
-    endIcon.setBounds(0, 0, 0, 0)
-
-//    val scale = ScaleDrawable(
-//      endIcon,
-//      Gravity.CENTER,
-//      1f,
-//      1f
-//    )
-//    scale.level = 10000
-
-//    endIcon.setBounds(
-//      0,
-//      10,
-//      (endIcon.intrinsicWidth * 0.3).toInt(),
-//      (endIcon.intrinsicHeight * 0.3).toInt()
-//    )
-//    basic.invalidateDrawable(endIcon)
-
-    basic.endIconDrawable = endIcon
+    basic.endIconView().scaleX = 0.5f
+    basic.endIconView().scaleY = 0.5f
   }
 }
---
+++ text-field/src/main/java/com/google/android/material/textfield/TextInputLayoutHelper.kt
@@ -0,0 +1,7 @@
+package com.google.android.material.textfield
+
+import com.google.android.material.internal.CheckableImageButton
+
+fun TextInputLayout.endIconView(): CheckableImageButton {
+  return endIconView
+}
