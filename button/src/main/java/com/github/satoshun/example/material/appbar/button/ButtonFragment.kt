abea075a422f4fc61becd947ab4cd6a41ec01373
SatoShun <shun.sato1@gmail.com>
2019-12-23 09:53:55 +0000

    add

--- button/src/main/java/com/github/satoshun/example/material/appbar/button/ButtonFragment.kt
+++ button/src/main/java/com/github/satoshun/example/material/appbar/button/ButtonFragment.kt
@@ -1,23 +1,5 @@
 package com.github.satoshun.example.material.appbar.button
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
-class ButtonFragment : BaseFragment() {
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View =
-    inflater.inflate(
-      R.layout.button_frag,
-      container,
-      false
-    )
-
-  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    super.onViewCreated(view, savedInstanceState)
-  }
-}
+class ButtonFragment : Fragment(R.layout.button_frag)
