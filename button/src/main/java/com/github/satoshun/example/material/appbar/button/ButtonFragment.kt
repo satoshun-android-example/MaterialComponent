b230fa1e27ec47d098013a13492ba570f47e1dfd
SatoShun <shun.sato1@gmail.com>
2019-12-24 00:33:02 +0000

    comment out

--- button/src/main/java/com/github/satoshun/example/material/appbar/button/ButtonFragment.kt
+++ button/src/main/java/com/github/satoshun/example/material/appbar/button/ButtonFragment.kt
@@ -1,5 +1,14 @@
 package com.github.satoshun.example.material.appbar.button
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 
-class ButtonFragment : Fragment(R.layout.button_frag)
+class ButtonFragment : Fragment(R.layout.button_frag) {
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+//    val model = ShapeAppearanceModel.builder().setAllCornerSizes(ShapeAppearanceModel.PILL).build()
+//    diamond.shapeAppearanceModel = model
+  }
+}
